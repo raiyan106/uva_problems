8fd62c2bf21db14afa35026babbf9d4d2d3b3b23
Raiyan Khan <raiyan.khan.106@gmail.com>
2020-05-05 23:38:57 +0600

    uva 11332

---
+++ uva_11332/uva_11332.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int recur_sum(int n);
+int main(){
+
+    int num;
+    while(scanf("%d", &num), (num)){
+        cout<<recur_sum(num)<<endl;
+    }
+
+}
+
+int recur_sum(int n){
+
+    int val = n;
+    int sum = 0;
+    if(val<10 && val>0){
+        return val;
+    }
+     while(val>0){
+        sum = sum + (val%10);
+        val/=10;
+    }
+     return recur_sum(sum);
+
+
+
+}
